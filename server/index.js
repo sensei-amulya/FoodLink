1b98d4b111e56ade55a7722face2fee50bbb7f97
anirudhgautam12 <gautamanirudh372@gmail.com>
2026-03-26 17:14:10 +0530

    authentication-modi

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ import connectDB from './config/db.js';
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5000;
 
 import http from 'http';
 import { Server } from 'socket.io';
