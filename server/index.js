77781014c2708249aad0388f1a6212c7f6c7a9c1
anirudhgautam12 <gautamanirudh372@gmail.com>
2026-03-26 20:02:14 +0530

    authentication-again

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ import { startCompostCron } from './cron.js';
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000 ;
 
 import http from 'http';
 import { Server } from 'socket.io';
